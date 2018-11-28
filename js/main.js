15710e6d628aea1100cf5e633334379d1a95aa44
RookieYang <yy335281965@gmail.com>
2018-11-28 23:34:37 +0800

    使用jQuery 优化代码

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-clickMe.addEventListener('click', function( ){
-  popover.style.display = 'block'
+
+$(clickMe).on('click',function(){
+  $(popover).show()
+  console.log('show')
+  setTimeout(function(){
+    console.log('添加one click')
+    $(document).one('click',function(){
+      console.log('hide')
+      $(popover).hide()
+    })
+  },0)
+ 
 })
 
-wrapper.addEventListener('click',function(e){
+$(wrapper).on('click',function(e){
   e.stopPropagation()
-})
-
-document.addEventListener('click',function(){
-  popover.style.display = 'none'
-
+  console.log('click事件走到了document')
 })
